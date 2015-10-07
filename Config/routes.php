6c9eb790a4b9e8808b65e249faec34fdf9c78e2a
David Yang <daworld.ny@gmail.com>
2015-10-08 04:24:48 +0500

    Migrated Pages module to standalone repository

---
+++ Config/routes.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * This file is part of the Bono CMS
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+return array(
+	
+	'/module/pages/(:var)' => array(
+		'controller' => 'Page@indexAction'
+	),
+
+	'/admin/module/pages/filter/(:var)' => array(
+		'controller' => 'Admin:Browser@filterAction'
+	),
+	
+	'/admin/module/pages' => array(
+		'controller' => 'Admin:Browser@indexAction',
+	),
+	
+	'/admin/module/pages/browse/(:var)' => array(
+		'controller' => 'Admin:Browser@indexAction'
+	),
+	
+	'/admin/module/pages/delete.ajax' => array(
+		'controller' => 'Admin:Browser@deleteAction',
+		'disallow' => array('guest')
+	),
+	
+	'/admin/module/pages/delete-selected.ajax' => array(
+		'controller' => 'Admin:Browser@deleteSelectedAction',
+		'disallow' => array('guest')
+	),
+	
+	'/admin/module/pages/save.ajax' => array(
+		'controller' => 'Admin:Browser@saveAction',
+		'disallow' => array('guest')
+	),
+	
+	'/admin/module/pages/add' => array(
+		'controller' => 'Admin:Add@indexAction'
+	),
+	
+	'/admin/module/pages/add.ajax' => array(
+		'controller' => 'Admin:Add@addAction',
+		'disallow' => array('guest')
+	),
+	
+	'/admin/module/pages/edit/(:var)' => array(
+		'controller' => 'Admin:Edit@indexAction'
+	),
+	
+	'/admin/module/pages/edit.ajax' => array(
+		'controller' => 'Admin:Edit@updateAction',
+		'disallow' => array('guest')
+	)
+);
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Pages
 Pages module
+============
+
+Pages module is a core module of your web site. It lets you to attach pages with different controllers and layouts. For details, please refer its documentation.
