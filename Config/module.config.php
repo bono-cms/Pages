2e508352e0378499b778935ea09f8577ad78348a
David Yang <daworld.ny@gmail.com>
2016-04-30 22:37:05 +0500

    Replaced raw path with route

--- Config/module.config.php
+++ Config/module.config.php
@@ -2,8 +2,8 @@
 
 return array(
     'module' => array(
-        'name' => 'Pages',
-        'url' => '/admin/module/pages',
+        'name'  => 'Pages',
+        'route' => 'Pages:Admin:Browser@indexAction',
         'icon' => 'fa fa-file-code-o fa-5x',
         'order' => 1,
         'description' => 'Pages module allows you to manage static pages on your site',
