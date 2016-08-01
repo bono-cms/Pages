747315c2324a2d348c8017f916e913ec8c6eab24
David Yang <daworld.ny@gmail.com>
2016-08-01 22:28:49 +0500

    Removed extras from module configuration file

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,11 +1,9 @@
 <?php
 
 return array(
-    'module' => array(
-        'name'  => 'Pages',
-        'route' => 'Pages:Admin:Browser@indexAction',
-        'icon' => 'fa fa-file-code-o fa-5x',
-        'order' => 1,
-        'description' => 'Pages module allows you to manage static pages on your site',
-    )
+ 'name'  => 'Pages',
+ 'route' => 'Pages:Admin:Browser@indexAction',
+ 'icon' => 'fa fa-file-code-o fa-5x',
+ 'order' => 1,
+ 'description' => 'Pages module allows you to manage static pages on your site',
 );
