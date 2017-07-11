a22c65f30427663bd4f822c50827f0acb1a1f0e8
David Yang <daworld.ny@gmail.com>
2017-07-11 14:40:07 +0500

    Added missing translation table in dropper

--- Storage/MySQL/Dropper.php
+++ Storage/MySQL/Dropper.php
@@ -21,7 +21,8 @@ final class Dropper extends AbstractStorageDropper
     protected function getTables()
     {
         return array(
-            PageMapper::getTableName()
+            PageMapper::getTableName(),
+            PageMapper::getTranslationTable()
         );
     }
 }
