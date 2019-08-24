0fdb8cdcf6c4d8b9f75de285ab48b80c60c18d7e
dave-yang <daworld.ny@gmail.com>
2019-08-24 17:26:56 +0500

    Fixes in schema

--- Storage/MySQL/schema.sql
+++ Storage/MySQL/schema.sql
@@ -1,19 +1,17 @@
 
+/* Pages */
 DROP TABLE IF EXISTS `bono_module_pages`;
 CREATE TABLE `bono_module_pages` (
-
-	`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	`template` varchar(32) NOT NULL COMMENT 'Template override',
-	`protected` varchar(1) NOT NULL COMMENT 'Whether this page is allowed to be removed in simple mode',
-	`seo` varchar(1) NOT NULL COMMENT 'Whether it should be indexed in SEO',
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `template` varchar(32) NOT NULL COMMENT 'Template override',
+    `protected` varchar(1) NOT NULL COMMENT 'Whether this page is allowed to be removed in simple mode',
+    `seo` varchar(1) NOT NULL COMMENT 'Whether it should be indexed in SEO',
     `default` varchar(1) NOT NULL COMMENT 'Whether this page is considered default',
     `image` varchar(30) NOT NULL COMMENT 'Attached page image'
-
-) DEFAULT CHARSET = UTF8;
+) ENGINE = InnoDB DEFAULT CHARSET = UTF8;
 
 DROP TABLE IF EXISTS `bono_module_pages_translations`;
 CREATE TABLE `bono_module_pages_translations` (
-
     `id` INT NOT NULL,
     `lang_id` INT NOT NULL COMMENT 'Language identificator',
     `web_page_id` INT NOT NULL COMMENT 'Web page identificator can be found in site module',
@@ -21,10 +19,12 @@ CREATE TABLE `bono_module_pages_translations` (
     `name` varchar(255) NOT NULL COMMENT 'Page name',
     `content` LONGTEXT NOT NULL COMMENT 'Fits for description',
     `keywords` TEXT NOT NULL,
-    `meta_description` TEXT NOT NULL COMMENT 'Meta-description for search engines'
+    `meta_description` TEXT NOT NULL COMMENT 'Meta-description for search engines',
 
-) DEFAULT CHARSET = UTF8;
-
+    FOREIGN KEY (id) REFERENCES bono_module_pages(id) ON DELETE CASCADE,
+    FOREIGN KEY (lang_id) REFERENCES bono_module_cms_languages(id) ON DELETE CASCADE,
+    FOREIGN KEY (web_page_id) REFERENCES bono_module_cms_webpages(id) ON DELETE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = UTF8;
 
 /* Extra fields */
 DROP TABLE IF EXISTS `bono_module_pages_extra_fields_cat_rel`;
@@ -34,7 +34,7 @@ CREATE TABLE `bono_module_pages_extra_fields_cat_rel` (
 
     FOREIGN KEY (master_id) REFERENCES bono_module_pages(id) ON DELETE CASCADE,
     FOREIGN KEY (slave_id) REFERENCES bono_module_block_categories(id) ON DELETE CASCADE
-);
+) ENGINE = InnoDB DEFAULT CHARSET = UTF8;
 
 DROP TABLE IF EXISTS `bono_module_pages_extra_fields`;
 CREATE TABLE `bono_module_pages_extra_fields` (
@@ -45,7 +45,7 @@ CREATE TABLE `bono_module_pages_extra_fields` (
     
     FOREIGN KEY (page_id) REFERENCES bono_module_pages(id) ON DELETE CASCADE,
     FOREIGN KEY (field_id) REFERENCES bono_module_block_category_fields(id) ON DELETE CASCADE
-);
+) ENGINE = InnoDB DEFAULT CHARSET = UTF8;
 
 DROP TABLE IF EXISTS `bono_module_pages_extra_fields_translations`;
 CREATE TABLE `bono_module_pages_extra_fields_translations` (
@@ -53,5 +53,6 @@ CREATE TABLE `bono_module_pages_extra_fields_translations` (
     `lang_id` INT NOT NULL COMMENT 'Language identificator',
     `value` varchar(255) NOT NULL,
 
-    FOREIGN KEY (id) REFERENCES bono_module_pages_extra_fields(id) ON DELETE CASCADE
-);
+    FOREIGN KEY (id) REFERENCES bono_module_pages_extra_fields(id) ON DELETE CASCADE,
+    FOREIGN KEY (lang_id) REFERENCES bono_module_cms_languages(id) ON DELETE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET = UTF8;
